74132a6da8100b2073f9ccde2286842843d653ab
Flavius Stefan <flaviusstefanungureanu@gmail.com>
2024-03-02 16:27:13 +0200

    Created POST Categories action method

--- CodePulse.API/Models/DTO/CategoryDto.cs
+++ CodePulse.API/Models/DTO/CategoryDto.cs
@@ -1,9 +1,10 @@
-﻿namespace CodePulse.API.Models.Domain
+﻿namespace CodePulse.API.Models.DTO
 {
-    public class Category
+    public class CategoryDto
     {
         public Guid Id { get; set; }
         public string Name { get; set; }
         public string UrlHandle { get; set; }
+
     }
 }
