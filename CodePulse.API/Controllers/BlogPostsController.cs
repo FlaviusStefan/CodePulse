61587b9bc06df644f4ace0a9e5d46242fa14f517
Flavius Stefan <flaviusstefanungureanu@gmail.com>
2024-03-14 15:58:56 +0200

    API - changed "AddBlogPost" action method - accept categories

--- CodePulse.API/Controllers/BlogPostsController.cs
+++ CodePulse.API/Controllers/BlogPostsController.cs
@@ -12,10 +12,12 @@ namespace CodePulse.API.Controllers
     public class BlogPostsController : ControllerBase
     {
         private readonly IBlogPostRepository blogPostRepository;
+        private readonly ICategoryRepository categoryRepository;
 
-        public BlogPostsController(IBlogPostRepository blogPostRepository)
+        public BlogPostsController(IBlogPostRepository blogPostRepository, ICategoryRepository categoryRepository)
         {
             this.blogPostRepository = blogPostRepository;
+            this.categoryRepository = categoryRepository;
         }
         // POST: {apibaseurl}/api/blogposts
         [HttpPost]
@@ -32,7 +34,17 @@ namespace CodePulse.API.Controllers
                 ShortDescription = request.ShortDescription,
                 Title = request.Title,
                 UrlHandle = request.UrlHandle,
+                Categories = new List<Category>()
             };
+
+            foreach(var categoryGuid in request.Categories) 
+            {
+                var existingCategory = await categoryRepository.GetById(categoryGuid);
+                if(existingCategory is not null)
+                {
+                    blogPost.Categories.Add(existingCategory);
+                }
+            }
 
             blogPost = await blogPostRepository.CreateAsync(blogPost);
 
@@ -40,14 +52,20 @@ namespace CodePulse.API.Controllers
             var response = new BlogPostDto
             {
                 Id = blogPost.Id,
-                Author = request.Author,
-                Content = request.Content,
-                FeaturedImageUrl = request.FeaturedImageUrl,
-                IsVisible = request.IsVisible,
-                PublishedDate = request.PublishedDate,
-                ShortDescription = request.ShortDescription,
-                Title = request.Title,
-                UrlHandle = request.UrlHandle,
+                Author = blogPost.Author,
+                Content = blogPost.Content,
+                FeaturedImageUrl = blogPost.FeaturedImageUrl,
+                IsVisible = blogPost.IsVisible,
+                PublishedDate = blogPost.PublishedDate,
+                ShortDescription = blogPost.ShortDescription,
+                Title = blogPost.Title,
+                UrlHandle = blogPost.UrlHandle,
+                Categories = blogPost.Categories.Select(x => new CategoryDto
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    UrlHandle = x.UrlHandle
+                }).ToList()
             };
 
             return Ok(response);
