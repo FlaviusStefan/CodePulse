0104515a7c68ac69f08d1eff5a4baed0e36519e9
Flavius Stefan <flaviusstefanungureanu@gmail.com>
2024-03-23 03:01:47 +0200

    API - Image Repository

--- CodePulse.API/Controllers/ImagesController.cs
+++ CodePulse.API/Controllers/ImagesController.cs
@@ -1,4 +1,6 @@
 ﻿using CodePulse.API.Models.Domain;
+using CodePulse.API.Models.DTO;
+using CodePulse.API.Repositories.Interface;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,32 +10,58 @@ namespace CodePulse.API.Controllers
     [ApiController]
     public class ImagesController : ControllerBase
     {
+        private readonly IImageRepository imageRepository;
+
+        public ImagesController(IImageRepository imageRepository)
+        {
+            this.imageRepository = imageRepository;
+        }
+
         // POST: {apibaseurl}/api/images
         [HttpPost]
-        public async Task<IActionResult> UploadImage([FromForm] IFormFile file, 
-                                                     [FromForm] string fileName, 
+        public async Task<IActionResult> UploadImage([FromForm] IFormFile file,
+                                                     [FromForm] string fileName,
                                                      [FromForm] string title)
         {
             ValidateFileUpload(file);
 
-            if(ModelState.IsValid) 
+            if (ModelState.IsValid)
             {
                 // File upload
                 var blogImage = new BlogImage
                 {
-                    FileExtension = Path.GetExtension(fileName).ToLower(),
+                    FileExtension = Path.GetExtension(file.FileName).ToLower(),
                     FileName = fileName,
                     Title = title,
-                    DateCreated = DateTime.Now,
+                    DateCreated = DateTime.Now
                 };
+
+                blogImage = await imageRepository.Upload(file, blogImage);
+
+                // Convert Domain Model to DTO
+
+                var response = new BlogImageDto
+                {
+                    Id = blogImage.Id,
+                    Title = blogImage.Title,
+                    DateCreated = blogImage.DateCreated,
+                    FileExtension = blogImage.FileExtension,
+                    FileName = blogImage.FileName,
+                    Url = blogImage.Url
+                };
+
+                return Ok(response);
+
             }
+
+            return BadRequest(ModelState);
         }
 
         private void ValidateFileUpload(IFormFile file) 
         {
             var allowedExtensions = new string[] { ".jpg", ".jpeg", ".png" };
 
-            if(allowedExtensions.Contains(Path.GetExtension(file.FileName).ToLower())) 
+            if(!allowedExtensions.Contains(Path.GetExtension(file.FileName).ToLower())) 
             {
                 ModelState.AddModelError("file", "Unsupported file format");
             }
